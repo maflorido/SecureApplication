6c36731bc487c18f1c1b3b3800c937fef0a6d7cf
y8p9 <marcello.amaral1@hotmail.com>
2015-12-02 16:10:50 -0200

    implementação do identity

--- SecureApp/SecureApp.Web/Controllers/HomeController.cs
+++ SecureApp/SecureApp.Web/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using SecureApp.Web.Models.Home;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace SecureApp.Web.Controllers
 {
@@ -10,6 +12,31 @@ namespace SecureApp.Web.Controllers
     {
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel viewModel, string returnUrl = null)
+        {
+            
+
+            if (Membership.ValidateUser(viewModel.UserName, viewModel.Password))
+            {
+                FormsAuthentication.SetAuthCookie(viewModel.UserName, viewModel.RememberMe);
+
+                if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/") && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "The user name or password provided is incorrect");
+            }
             return View();
         }
         
