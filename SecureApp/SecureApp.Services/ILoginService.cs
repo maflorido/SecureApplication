20bb9fa72e19e703fefd6e5497a2443b6a955017
y8p9 <marcello.amaral1@hotmail.com>
2015-12-03 15:21:01 -0200

    removendo namespaces desnecessarios.

--- SecureApp/SecureApp.Services/ILoginService.cs
+++ SecureApp/SecureApp.Services/ILoginService.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.Text;
+﻿using System.ServiceModel;
 
 namespace SecureApp.Services
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ILoginService" in both code and config file together.
     [ServiceContract]
     public interface ILoginService
     {
--- SecureApp/SecureApp.Services/LoginService.svc.cs
+++ SecureApp/SecureApp.Services/LoginService.svc.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.Text;
-using System.Web.Security;
+﻿using System.Web.Security;
 
 namespace SecureApp.Services
 {
