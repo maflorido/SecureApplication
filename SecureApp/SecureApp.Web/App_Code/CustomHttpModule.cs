f8f3fad2550fe976910d6f2ddc88d4f2a92e2214
y8p9 <marcello.amaral1@hotmail.com>
2015-12-04 11:14:48 -0200

    criacao http module

---
+++ SecureApp/SecureApp.Web/App_Code/CustomHttpModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SecureApp.Web
+{
+    public class CustomHttpModule : IHttpModule
+    {
+
+        public CustomHttpModule()
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += new EventHandler(OnBeginRequest);
+        }
+
+        public void OnBeginRequest(Object s, EventArgs e)
+        {
+            HttpApplication app = s as HttpApplication;
+            app.Context.Response.Write("Hello from OnBeginRequest in custom module.<br>");            
+        }
+    }
+}
---
+++ SecureApp/SecureAppModules/CustomHttpModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web;
+
+namespace SecureAppModules
+{
+    public class CustomHttpModule : IHttpModule
+    {
+
+        public CustomHttpModule()
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += new EventHandler(OnBeginRequest);
+        }
+
+        public void OnBeginRequest(Object s, EventArgs e)
+        {
+            HttpApplication app = s as HttpApplication;
+            
+        }
+    }
+}
